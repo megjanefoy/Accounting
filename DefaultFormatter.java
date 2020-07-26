76e5a1fac6bbb31f6d0df442e0cce0bdc46d861f
Megan Jane Thompson <59883929+megjthom@users.noreply.github.com>
2020-07-26 15:17:08 -0500

    Update DefaultFormatter.java

--- DefaultFormatter.java
+++ DefaultFormatter.java
@@ -1,11 +1,8 @@
 /** 
  * Title: Assignment 4
- * Semester: COP3337 – Fall 2019
+ * Semester: COP3337 â€“ Fall 2019
  * @author Megan Jane Thompson
  *
- * I affirm that this program is entirely my own work
- * and none of it is the work of any other person.
- * 
  * This program extends NumberFormatter interface 
  * and formats an integer the usual way.
 */
